895e88feb584bed78f32cb23406eac54c0febbf0
Mani Singh <nerdhusband786@gmail.com>
2026-05-21 15:56:04 -0700

    CartIQ Vercel ready

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CartIQ from './CartIQ.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CartIQ />
+  </React.StrictMode>,
+)
